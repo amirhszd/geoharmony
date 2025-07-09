4ccd0dd5858623809a74ac5d53a657b15d053a1b
Amir Hassanzadeh <ah7557@rit.edu>
2025-07-09 17:09:47 -0400

    refactor image_matchmaker: integrate new point picking and warped image display apps; enhance homography calculation process

--- geoharmony/tools/image_matchmaker/image_matchmaker.py
+++ geoharmony/tools/image_matchmaker/image_matchmaker.py
@@ -1,48 +1,118 @@
 # main.py
 import matplotlib.pyplot as plt
 import multiprocessing as mp
-from get_points_v2 import run_dash
+from get_points_app_v2 import run_get_points_app
+from show_warped_app import run_show_warped_app
 import numpy as np
 import os
 import cv2
 
 
-def calculate_homography_gui(ref_arr,
-                             ref_bands,
-                             ref_arr_uint8,
-                             target_arr,
-                             target_bands,
-                             target_arr_uint8):
+def launch_get_points_app(img1, img2):
+    mgr = mp.Manager()
+    q = mgr.Queue()
+    p = mp.Process(target=run_get_points_app, args=(img1, img2, q), daemon=True)
+    p.start()
+    return q, p
 
-    ###  THIS IS WHERE WE RUN APP GET POINTS APP
+def launch_show_warped_app(image):
+    mgr = mp.Manager()
+    q = mgr.Queue()
+    p = mp.Process(target=run_show_warped_app, args=(image, q), daemon=True)
+    p.start()
+    return q, p
 
 
+def calc_homography(ref_points, target_points):
     # point passed to homography should be x, y order
     ref_points = np.array(ref_points)
     target_points = np.array(target_points)
     M, mask = cv2.findHomography(ref_points, target_points, cv2.RANSAC, 5)
 
+    return M
 
-    #### THIS IS WHEN THE GET POINTS APP IS AND WE CLOSE IT
-
-
-    ### WE SHOW APP 2
-
+    # run a small app that shows the target image warped to the reference image and shows the result
+    # if the use likes it we return the homography matrix if not we restart the process
     ### APP2 BASICALLY WARPS THE TARGET IMAGE TO THE REFERENCE IMAGE USING THE HOMOGRAPHY MATRIX AND SHOWS IT TO THE USER
-    ax.imshow(mica_image_uint8, alpha=0.5)
-    ax.imshow(cv2.warpPerspective(hs_image_uint8, M, (mica_image.shape[1], mica_image.shape[0])),
-              alpha=0.5)
 
-    ### TWO BUTTON DOWN BELOW, GOOD TO GO! AND REPEAT! GOOD TO GO WOULD BE GREEN AND REPEAT WOULD BE RED
+    # Convert to float32 for accurate blending
+    mica_float = mica_image_uint8.astype(np.float32)
+    hs_float = hs_warped.astype(np.float32)
+    # Blend the two images with 50% transparency each
+    composite = 0.5 * mica_float + 0.5 * hs_float
+    # Clip and convert back to uint8
+    composite_uint8 = np.clip(composite, 0, 255).astype(np.uint8)
 
-    ### IF USER IS SATISFRIED WE RETURN THE HOMOGRAPHY MATRIX, CLOSE APP2
+    # launch the show warped app here
+    q, dash_process = launch_get_points_app(ref_image, target_image)
+    ref_pts, tgt_pts = q.get()
+    dash_process.join()
 
-    ## IF USE IS NOT SATIFIRTED WE CLOSE APP2 AND RESTART THE PROCESS
+
+
+    if not satisfied:
+        # we need to go back to the point picking GUI again so this function should return either a homography matrix or None
+        return None
+
+    # if the user is satisfied we return the homography matrix and save oout the image
+
+
+    ### write out saving out the warped image to file; incorporate in gdal image?
 
     return M
 
+def get_points_gui(ref_image, target_image):
+    """
+    This function launches the Dash app for manual point picking.
+    It uses multiprocessing to run the app in a separate process and returns the picked points.
 
-def coregister_manual(ref_gdalimage,
+    Parameters:
+        ref_image (numpy.ndarray): The reference image for point picking.
+        target_image (numpy.ndarray): The target image for point picking.
+
+    Returns:
+        tuple: A tuple containing two lists of points:
+               - ref_pts: Points picked on the reference image.
+               - tgt_pts: Points picked on the target image.
+
+    """
+
+    # launch GUI
+    q, dash_process = launch_get_points_app(ref_image, target_image)
+
+    # block until user clicks Submit
+    ref_pts, tgt_pts = q.get()
+    dash_process.join()
+
+    return ref_pts, tgt_pts
+
+def get_warped_gui(image):
+    """
+    This function launches the Dash app for manual point picking.
+    It uses multiprocessing to run the app in a separate process and returns the picked points.
+
+    Parameters:
+        ref_image (numpy.ndarray): The reference image for point picking.
+        target_image (numpy.ndarray): The target image for point picking.
+
+    Returns:
+        tuple: A tuple containing two lists of points:
+               - ref_pts: Points picked on the reference image.
+               - tgt_pts: Points picked on the target image.
+
+    """
+
+    # launch GUI
+    q, dash_process = launch_show_warped_app(image)
+
+    # block until user clicks Submit
+    response = q.get()
+    dash_process.join()
+
+    return response
+
+
+def coregister_gui(ref_gdalimage,
                       ref_bands,
                       target_gdalimage,
                       target_bands,
@@ -56,10 +126,6 @@ def coregister_manual(ref_gdalimage,
     homorgraphy is saved everytime is ran so it can be resused later.
     name is the  extension added at the end of the warped and coregistered iamge.
     """
-    # load the dada
-    ref_arr = ref_gdalimage.read()
-    target_arr = target_gdalimage.read()
-
     # lets generate unint 8 images for display
     ref_arr_uint8 = ref_gdalimage.read_display_image()
     target_arr_uint8 = target_gdalimage.read_display_image()
@@ -67,27 +133,42 @@ def coregister_manual(ref_gdalimage,
     # homography file name
     homography_path = os.path.join(outfolder, f"homography_{name}.npy")
 
-
     if os.path.exists(homography_path) and use_available_homography:
-        M = np.load(homography_path)
-    else:       # launch the point picker GUI
-        M = calculate_homography_manual(ref_arr,
-                                        ref_arr_uint8,
-                                        target_arr,
-                                        target_bands,
-                                        target_arr_uint8)
-        np.save(homography_path, M)
+        h_mat = np.load(homography_path)
+    else:
+        response =  False
+        while not response:
+            # run the point picking GUI and get the points
+            ref_points, target_points = get_points_gui(ref_arr_uint8, target_arr_uint8)
+
+            # calculate the homography matrix
+            h_mat = calc_homography(ref_points, target_points)
+
+            # warp the target image using the homography matrix
+            tareget_arr_warped = cv2.warpPerspective(target_arr_uint8, h_mat, (ref_arr_uint8.shape[1], ref_arr_uint8.shape[0]))
+
+            # blend the two images to show them with 50% transparency each
+            mica_float = ref_arr_uint8.astype(np.float32)
+            hs_float = tareget_arr_warped.astype(np.float32)
+            composite = 0.5 * mica_float + 0.5 * hs_float
+            composite_uint8 = np.clip(composite, 0, 255).astype(np.uint8)
+
+            # send the new image to the GUI to show the warped image, if the response is True we move on if not we repeat the process
+            response = get_warped_gui(composite_uint8)
+
+    # if successful we save the homography matrix to file
+    np.save(homography_path, h_mat)
+
+    # save the warped image to file using GDALimage
+    from geoharmony.tools.gdalwriter import save_crop_warp
+    target_gdalimage_warped = save_crop_warp(target_gdalImage = target_gdalimage,
+                                             ref_gdalImage = ref_gdalimage,
+                                             homography_matrix= h_mat)
+
+    # save the warped image to file
 
 
 
-
-
-def launch_point_picker(img1, img2):
-    mgr = mp.Manager()
-    q   = mgr.Queue()
-    p   = mp.Process(target=run_dash, args=(img1, img2, q), daemon=True)
-    p.start()
-    return q, p
 
 if __name__ == "__main__":
     # load your images
